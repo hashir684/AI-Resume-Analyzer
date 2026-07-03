fe0482d880bc0798b804a2c9f812d17cf5e1c131
Hashir <hashirmuhammad797@gmail.com>
2026-07-03 15:47:17 +0500

    Added correct URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8001/analyze-resume/"
+API_URL = "http://127.0.0.1:8000/analyze-resume/"
 
 st.set_page_config(
     page_title="AI Resume Analyzer",
